be273acc3c2bd92f81ae6012c67df93bea5aaf66
Olaf Conradi <olaf@conradi.org>
2014-05-08 00:26:39 +0200

    Setup ZODB tween with settings uri for databases

--- more/zodb/main.py
+++ more/zodb/main.py
@@ -1,17 +1,32 @@
 import morepath
 
+from zodburi import resolve_uri
+from ZODB import DB
+from ZODB.ActivityMonitor import ActivityMonitor
+
 
 app = morepath.App()
 
 
-@app.setting(section='zodb', name='zodbconn_uri')
-def get_zodbconn_uri():
-    return 'file://%(here)s/Data.fs?connection_cache_size=20000'
+@app.setting(section='zodb', name='primary')
+def get_primary_uri():
+    return 'file://Data.fs?connection_cache_size=20000'
+
+
+def db_from_uri(uri, dbname, dbmap, resolve_uri=resolve_uri):
+    storage_factory, dbkw = resolve_uri(uri)
+    dbkw['database_name'] = dbname
+    storage = storage_factory()
+    return DB(storage, databases=dbmap, **dbkw)
 
 
 @app.tween_factory()
 def zodb_tween_factory(app, handler):
-    uri = app.settings.zodb.zodbconn_uri
+    # TODO Register databases somewhere
+    databases = {}
+    for name, uri in vars(app.settings.zodb).items():
+        db = db_from_uri(uri, name, databases)
+        db.setActivityMonitor(ActivityMonitor())
 
     def zodb_tween(request):
         response = handler(request)
